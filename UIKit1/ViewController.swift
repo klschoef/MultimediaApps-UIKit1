4f30540e21cb12b94c28f292672fa9e5fcad2df9
klschoef <ks@itec.aau.at>
2019-10-13 19:29:44 +0200

    Initial Commit

---
+++ UIKit1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKit1
+//
+//  Created by Klaus Schoeffmann on 13.10.19.
+//  Copyright © 2019 Klaus Schoeffmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
