4ee4b5d84c637ae48b034105fa70c9086336b7f5
klschoef <ks@itec.aau.at>
2019-10-13 20:25:09 +0200

    working version

--- UIKit1/ViewController.swift
+++ UIKit1/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var innerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if let outerView = innerView.superview {
+            outerView.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        }
     }
 
-
+    @IBAction func changeBounds(_ sender: Any) {
+        self.view.bounds = CGRect(x: -100,y: -200, width: self.view.bounds.width, height: self.view.bounds.height)
+    }
+    
+    @IBAction func resetBounds(_ sender: Any) {
+        self.view.bounds = CGRect(origin: CGPoint.zero, size: self.view.bounds.size)
+    }
+    
+    @IBAction func changeBoundsToRight(_ sender: Any) {
+        self.view.bounds = CGRect(origin: CGPoint(x: 50, y: 100), size: self.view.bounds.size)
+    }
 }
 
